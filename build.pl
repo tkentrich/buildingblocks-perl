9fbde864300f09e63a72f240a53cb6d7d0d7027b
tkentrich <tkentrich@gmail.com>
2016-09-04 07:24:04 -0600

    Create build.pl

---
+++ build.pl
@@ -0,0 +1,55 @@
+			default:
+			case 1:
+				SetSize(8, 4, 8);
+				Add(0, new string[] {
+					"  BXXXCX",
+					" B XXXX ",
+					" X XBX  ",
+					" X X X  ",
+					" X X X  ",
+					"    B  C",
+					" XXXXX  ",
+					"        ",
+				});
+				Add(1, new string[] {
+					"   XXX B",
+					" B XXXX ",
+					" XXX X  ",
+					" XX     ",
+					" XXXXX  ",
+					" X   X  ",
+					" XXXXX  ",
+					"        ",
+				});
+				Add(2, new string[] {
+					"   XXXX ",
+					"   XXXX ",
+					" X X X  ",
+					" XX X XX",
+					" XXXXX  ",
+					" X   X  ",
+					" XXXXX  ",
+					"        ",
+				});
+				Add(3, new string[] {
+					"     XX ",
+					"  X XXX ",
+					" XX  XX ",
+					" X     O",
+					" X X    ",
+					"        ",
+					" XXXXX  ",
+					"        ",
+				});
+			case 2:
+				SetSize(?,?,?);
+				Add(0, new string[] {
+					"  BXXXCX",
+					" B XXXX ",
+					" X XBX  ",
+					" X X X  ",
+					" X X X  ",
+					"    B  C",
+					" XXXXX  ",
+					"        ",
+				});
