f8c58993a01e10c99cbf8c79b7c8defc357f38ea
Cloud User <ec2-user@ip-172-31-58-159.ec2.internal>
2016-09-11 10:28:03 -0400

    Update Run.pl

--- Run.pl
+++ Run.pl
@@ -1,7 +1,19 @@
 #!/usr/bin/perl
 
 use strict;
+use Term::ReadKey;
 
+my ($TRUE, $FALSE) = (1 == 1, 1 == 0);
 my @level;
+my $key;
+my $gotKey;
+my $playing = $TRUE;
 
-
+while ($playing == $TRUE) {
+	ReadMode 4;
+	while (not defined ($key = ReadKey(-1))) {
+	}
+	$key = ord($key);
+	print "Get key $key\n";
+	ReadMode 0;
+}
